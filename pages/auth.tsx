557a0fe5bc38137a45bc7fa5e4314e91d03a4ec2
baranero <jakub.baran2@gmail.com>
2023-05-06 21:56:43 +0200

    added input component

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,18 +1,26 @@
+import Input from "@/components/Input"
 import Image from "next/image"
 
 const Auth = () => {
     return (
         <div className="relative h-full w-full bg-[url('/images/background.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
             <div className="bg-black w-full h-full lg:bg-opacity-50">
-                <nav>
+                <nav className="px-12 py-5">
                 <Image
                         src="/images/logo.png"
                         alt="Logo"
                         width={200}
                         height={100}
-                        className="ml-10"
                     />
                 </nav>
+                <div className="flex justify-center">
+                    <div className="bg-zinc-700 bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+                        <h2 className="text-white text-4xl mb-8 font-semibold">Sign in</h2>
+                        <div className="flex flex-col gap-4">
+                            <Input/>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
