7c445f00a7869c99bda045330982ee7f98e4b624
baranero <jakub.baran2@gmail.com>
2023-05-06 23:22:52 +0200

    input design

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,7 +1,11 @@
 import Input from "@/components/Input"
 import Image from "next/image"
+import { useState } from "react"
 
 const Auth = () => {
+
+    const [email, setEmail] = useState('')
+
     return (
         <div className="relative h-full w-full bg-[url('/images/background.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
             <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -17,7 +21,13 @@ const Auth = () => {
                     <div className="bg-zinc-700 bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
                         <h2 className="text-white text-4xl mb-8 font-semibold">Sign in</h2>
                         <div className="flex flex-col gap-4">
-                            <Input/>
+                            <Input
+                                label="Email"
+                                onChange={() => {}}
+                                id="email"
+                                type="email"
+                                value=""
+                            />
                         </div>
                     </div>
                 </div>
