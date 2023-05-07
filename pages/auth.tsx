c80e76142883b70f8a6b9fee67476701fc21ff61
baranero <jakub.baran2@gmail.com>
2023-05-07 09:16:14 +0200

    added another inputs and login button

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -5,6 +5,8 @@ import { useState } from "react"
 const Auth = () => {
 
     const [email, setEmail] = useState('')
+    const [name, setName] = useState('')
+    const [password, setPassword] = useState('')
 
     return (
         <div className="relative h-full w-full bg-[url('/images/background.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -19,16 +21,32 @@ const Auth = () => {
                 </nav>
                 <div className="flex justify-center">
                     <div className="bg-zinc-700 bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-                        <h2 className="text-white text-4xl mb-8 font-semibold">Sign in</h2>
+                        <h2 className="text-white text-4xl mb-8 text-center font-semibold">Sign in</h2>
                         <div className="flex flex-col gap-4">
+                        <Input
+                                label="Username"
+                                onChange={(event: any) => setName(event.target.value)}
+                                id="name"
+                                type="text"
+                                value={name}
+                            />
                             <Input
                                 label="Email"
-                                onChange={() => {}}
+                                onChange={(event: any) => setEmail(event.target.value)}
                                 id="email"
                                 type="email"
-                                value=""
+                                value={email}
+                            />
+                            <Input
+                                label="Password"
+                                onChange={(event: any) => setPassword(event.target.value)}
+                                id="password"
+                                type="password"
+                                value={password}
                             />
                         </div>
+                        <button className="bg-orange-600 py-3 text-white rounded-md w-full mt-10 hover:bg-orange-700 transition">Login</button>
+                        <p className="text-neutral-400 mt-8">Don&apos;t have an account? <span className="text-white ml-1 hover:underline cursor-pointer">Sign up</span></p>
                     </div>
                 </div>
             </div>
