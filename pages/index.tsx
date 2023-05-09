3a2db0c19ae7308acead1e5a318f87440e8a77d2
baranero <jakub.baran2@gmail.com>
2023-05-09 20:13:43 +0200

    state managment with useswr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
+import useCurrentUser from '@/hooks/useCurrentUser'
+import { signOut, getSession } from 'next-auth/react'
+import { NextPageContext } from 'next/types'
+
+export async function getServerSideProps(context: NextPageContext) {
+    const session = await getSession(context)
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/auth',
+                permanent: false
+            }
+        }
+    }
+
+    return {
+        props: {}
+    }
+}
+
 export default function Home() {
+
+    const { data: user } = useCurrentUser()
+    
+
     return (
         <>
             <h1>Fire App</h1>
+            <button onClick={() => signOut()}>Sign Out</button>
         </>
     )
 
