a97d951a4e0355769f8616c298b4ecefabe3210e
baranero <jakub.baran2@gmail.com>
2023-05-17 08:47:35 +0200

    responsive main page

--- components/Description.tsx
+++ components/Description.tsx
@@ -10,15 +10,15 @@ interface DescriptionProps {
 
 const Description: React.FC<DescriptionProps> = ({ title, text, src, alt }) => {
     return (
-        <div className='text-white mt-10 py-4 flex flex-col items-center justify-center bg-neutral-600 bg-opacity-25 rounded-lg'>
-        <div className='w-full order-last px-3'>
+        <div className='text-white mt-10 py-4 md:p-0 lg:p-0 flex flex-col md:flex-row lg:flex-row lg:w-[70%] mx-auto items-center justify-center bg-neutral-600 bg-opacity-25 rounded-lg'>
+        <div className='w-full lg:w-[50%] order-last md:order-first lg:order-first px-3'>
             <h1 className='text-white text-center text-4xl my-6'>
                 {title}
             </h1>
             <p className='text-white text-center w-full text-2xl leading-relaxed opacity-75'>{text}
             </p>
         </div>
-        <img src={src} className='w-full opacity-70' alt={alt} />
+        <img src={src} className='w-full h-full object-cover lg:rounded-xl md:rounded-xl lg:w-[50%] md:w-[50%] opacity-70' alt={alt} />
     </div>
 
     )
