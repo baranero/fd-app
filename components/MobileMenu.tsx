c3b8991b5d3e5a465e1ddd1b537db18a19354021
baranero <jakub.baran2@gmail.com>
2023-05-17 20:18:57 +0200

    mobile logout button

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NavbarItem from "./NavbarItem";
 import Link from "next/link";
+import { signOut } from "next-auth/react";
 
 interface MobileMenuProps {
     visible?: boolean;
@@ -13,7 +14,8 @@ const MobileMenu: React.FC<MobileMenuProps> = ({ visible }) => {
     }
 
     return (
-        <div className="bg-black text-center w-full top-8 right-0 py-5 flex-col bordr-2 border-gray-800 flex">
+        <div className="bg-neutral-400
+        bg-opacity-20 text-center w-full top-8 right-0 py-5 flex-col bordr-2 border-gray-800 flex">
             <div className="flex flex-col gap-4">
                 <Link href='/'>
                     <NavbarItem label="Home"/>
@@ -27,6 +29,7 @@ const MobileMenu: React.FC<MobileMenuProps> = ({ visible }) => {
                             <Link href='/users'>
                                 <NavbarItem label="Users"/>
                             </Link>
+                            <p className="text-white cursor-pointer hover:text-gray-300 transition z-50" onClick={() => signOut()}>Logout</p>
 
             </div>
         </div>
