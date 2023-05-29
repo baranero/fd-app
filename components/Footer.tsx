a12340200769c64685957bd2ff3aa5d12886d0cf
baranero <jakub.baran2@gmail.com>
2023-05-29 15:23:01 +0200

    links changes in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,11 @@
-import Link from "next/link"
-import { AiFillGithub, AiFillLinkedin } from "react-icons/ai"
+import Link from "next/link";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
 const Footer = () => {
-
-    return (
-        <nav className="w-full z-40">
-            <div className="
+  return (
+    <nav className="w-full z-40">
+      <div
+        className="
               px-4
               md:px-3
               lg:px-16
@@ -18,25 +18,39 @@ const Footer = () => {
               duration-500
               bg-zinc-700
               bg-opacity-40
-            ">
-                <p className="text-white">&#169; 2023 Jakub Baran | All rights reserved.</p>
-                <div className="flex flex-row mt-3 w-full justify-center items-center">
-                    <Link href="https://github.com/baranero">
-                        <p className="text-white mx-2"><AiFillGithub size={30}/></p>
-                    </Link>
-                    <Link href="https://www.linkedin.com/in/jakub-baran-42a00522b/">
-                        <p className="text-white mx-2"><AiFillLinkedin size={30}/></p>
-                    </Link>
-                </div>
-                <div className="flex text-white flex-row mt-3 w-full justify-center items-center">
-                Image by <a className="hover:text-gray-400" href="https://www.freepik.com/free-photo/top-view-desk-arrangement-with-notebook_16688666.htm#query=schedule%20draw&position=34&from_view=search&track=ais">Freepik</a>
+            "
+      >
+        <p className="text-white">
+          &#169; 2023 Jakub Baran | All rights reserved.
+        </p>
+        <div className="flex flex-row mt-3 w-full justify-center items-center">
+          <Link href="https://github.com/baranero">
+            <p className="text-white mx-2">
+              <AiFillGithub size={30} />
+            </p>
+          </Link>
+          <Link href="https://www.linkedin.com/in/jakub-baran-42a00522b/">
+            <p className="text-white mx-2">
+              <AiFillLinkedin size={30} />
+            </p>
+          </Link>
+        </div>
+        <div className="flex text-white flex-row mt-3 w-full justify-center items-center">
+          Image by&nbsp;
+          <a
+            className="hover:text-gray-400"
+            href="https://www.freepik.com/free-photo/top-view-desk-arrangement-with-notebook_16688666.htm#query=schedule%20draw&position=34&from_view=search&track=ais"
+          > Freepik
+          </a>{" "}
+          &nbsp;&nbsp;| &nbsp;{" "}
+          <a className="hover:text-gray-400" href="https://www.pexels.com/pl-pl/zdjecie/czarny-niebieski-i-czerwony-wykres-ilustracja-186461/">
+            {" "}
+            Image added by Burak The Weekender
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-                </div>
-                
-            </div>
-
-        </nav>
-    )
-}
-
-export default Footer
+export default Footer;
