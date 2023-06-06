d2423a406480e610de7c7ea31ed42a0db13493ec
baranero <jakub.baran2@gmail.com>
2023-06-06 22:32:43 +0200

    Image component instead img tag

--- components/Description.tsx
+++ components/Description.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/legacy/image";
 
 interface DescriptionProps {
   title: string;
@@ -10,17 +11,25 @@ interface DescriptionProps {
 const Description: React.FC<DescriptionProps> = ({ title, text, src, alt }) => {
   return (
     <div className="text-white mt-10 py-4 md:p-0 lg:p-0 flex flex-col md:flex-row lg:flex-row lg:w-[70%] mx-auto items-center justify-center bg-neutral-600 bg-opacity-25 rounded-lg">
-      <div className="w-full lg:w-[50%] order-last md:order-first lg:order-first px-3">
+      <div className="w-full md:w-1/2 lg:w-1/2 order-last md:order-first lg:order-first px-3">
         <h1 className="text-white text-center text-4xl my-6">{title}</h1>
-        <p className="text-white text-center w-full text-2xl leading-relaxed opacity-75">
+        <p className="text-white text-center text-2xl leading-relaxed opacity-75">
           {text}
         </p>
       </div>
-      <img
-        src={src}
-        className="w-full h-full object-cover lg:rounded-xl md:rounded-xl lg:w-[50%] md:w-[50%] opacity-70"
-        alt={alt}
-      />
+      <div className="w-full h-full lg:rounded-xl md:rounded-xl lg:w-1/2 md:w-1/2 relative">
+        <div className="relative h-full w-full">
+          <Image
+            src={src}
+            alt={alt}
+            layout="responsive"
+            width={1200}
+            height={800}
+            objectFit="cover"
+            className="rounded-lg"
+          />
+        </div>
+      </div>
     </div>
   );
 };
