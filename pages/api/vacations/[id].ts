c6fa23b10be6ffa32780a9c103341ee4b3611e30
baranero <jakub.baran2@gmail.com>
2023-06-06 22:19:22 +0200

    increasing api/vacations/[id] performance

--- pages/api/vacations/[id].ts
+++ pages/api/vacations/[id].ts
@@ -6,24 +6,28 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  await serverAuth(req, res);
-  if (req.method === "DELETE") {
-    const id = Array.isArray(req.query.id) ? req.query.id[0] : req.query.id;
-    console.log(req.query);
-    try {
-      console.log(req.body);
+  try {
+    await serverAuth(req, res);
 
-      const deletedVacations = await prismadb.vacations.delete({
-        where: {
-          id: id,
-        },
-      });
-      return res.status(200).json(deletedVacations);
-    } catch (error) {
-      console.log({ error });
-      return res.status(500).end();
+    if (req.method !== "DELETE") {
+      return res.status(405).json({ error: "Method Not Allowed" });
     }
-  } else {
-    res.status(405).json({ error: "Method Not Allowed" });
+
+    const id = Array.isArray(req.query.id) ? req.query.id[0] : req.query.id;
+
+    if (!id) {
+      return res.status(400).json({ error: "Missing ID parameter" });
+    }
+
+    const deletedVacations = await prismadb.vacations.delete({
+      where: {
+        id: id,
+      },
+    });
+
+    return res.status(200).json(deletedVacations);
+  } catch (error) {
+    console.log({ error });
+    return res.status(500).end();
   }
 }
