c120215ac19fb8016400dc0eaa9e5350989c44ec
baranero <jakub.baran2@gmail.com>
2023-06-06 22:18:20 +0200

    increasing api/vacations performance

--- pages/api/vacations/index.ts
+++ pages/api/vacations/index.ts
@@ -6,45 +6,49 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  await serverAuth(req, res);
-  if (req.method === "GET") {
-    try {
-      const vacations = await prismadb.vacations.findMany();
+  try {
+    await serverAuth(req, res);
 
+    if (req.method === "GET") {
+      const vacations = await prismadb.vacations.findMany();
       return res.status(200).json(vacations);
-    } catch (error) {
-      console.log({ error });
-      return res.status(500).end();
     }
-  }
 
-  if (req.method === "POST") {
-    const { amount, name, type } = req.body;
+    if (req.method === "POST") {
+      const { amount, name, type } = req.body;
 
-    try {
-      const vacations = await prismadb.vacations.findMany();
+      if (!amount || !name || !type) {
+        return res.status(400).json({ error: "Amount, name, and type are required" });
+      }
 
       const user = await prismadb.firefighters.findUnique({
         where: {
           name: name,
         },
+        select: {
+          id: true,
+        },
       });
+
+      if (!user) {
+        return res.status(400).json({ error: "User not found" });
+      }
 
       const addedVacations = await prismadb.vacations.create({
         data: {
           id: (Date.now() + Math.random()).toString(),
           amount: +amount,
-          type,
-          userId: user!.id,
+          type: type,
+          userId: user.id,
         },
       });
 
       return res.status(200).json(addedVacations);
-    } catch (error) {
-      console.log({ error });
-      return res.status(500).end();
     }
-  } else {
-    res.status(405).json({ error: "Method Not Allowed" });
+
+    return res.status(405).json({ error: "Method Not Allowed" });
+  } catch (error) {
+    console.log({ error });
+    return res.status(500).end();
   }
 }
