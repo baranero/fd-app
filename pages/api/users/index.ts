8646e00f7318eaae6ec6f40fc27864bd45432e94
baranero <jakub.baran2@gmail.com>
2023-06-06 22:05:13 +0200

    increasing api/user performance

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -6,37 +6,34 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  await serverAuth(req, res);
-  if (req.method === "GET") {
-    try {
+  try {
+    await serverAuth(req, res);
+
+    if (req.method === "GET") {
       const firefighters = await prismadb.firefighters.findMany();
-
       return res.status(200).json(firefighters);
-    } catch (error) {
-      console.log({ error });
-      return res.status(500).end();
     }
-  }
 
-  if (req.method === "POST") {
-    const { name } = req.body;
+    if (req.method === "POST") {
+      const { name } = req.body;
 
-    try {
-      const user = await prismadb.firefighters.findMany();
+      if (!name) {
+        return res.status(400).json({ error: "Name is required" });
+      }
 
-      const addedFirefighters = await prismadb.firefighters.create({
+      const addedFirefighter = await prismadb.firefighters.create({
         data: {
           id: (Date.now() + Math.random()).toString(),
           name,
         },
       });
 
-      return res.status(200).json(addedFirefighters);
-    } catch (error) {
-      console.log({ error });
-      return res.status(500).end();
+      return res.status(200).json(addedFirefighter);
     }
-  } else {
-    res.status(405).json({ error: "Method Not Allowed" });
+
+    return res.status(405).json({ error: "Method Not Allowed" });
+  } catch (error) {
+    console.log({ error });
+    return res.status(500).end();
   }
 }
