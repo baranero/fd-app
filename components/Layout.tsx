35a011558b0976d23af7c06625116baf5ca36617
baranero <jakub.baran2@gmail.com>
2023-06-12 13:55:00 +0200

    Loading effect

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,10 @@
-import React, { ReactNode, useCallback } from "react";
+import React, { ReactNode, useCallback, useState, useEffect } from "react";
 import Navbar from "./Navbar";
 import SettingsModal from "./SettingsModal";
 import useSettingsModal from "@/hooks/useSettingsModal";
 import { mutate } from "swr";
 import Footer from "./Footer";
+import { useRouter } from "next/router";
 
 type Props = {
   children: ReactNode;
@@ -11,17 +12,46 @@ type Props = {
 
 const Layout: React.FC<Props> = (props: Props) => {
   const { isOpen, closeModal, openModal } = useSettingsModal();
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleOpenModal = useCallback(() => {
     mutate;
     openModal();
   }, [openModal]);
+
+  const router = useRouter();
+  const [pageLoading, setPageLoading] = useState<boolean>(false);
+  useEffect(() => {
+    const handleStart = () => {
+      setPageLoading(true);
+    };
+    const handleComplete = () => {
+      setPageLoading(false);
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+  }, [router]);
+
   return (
     <>
+      <Navbar openModal={handleOpenModal} />
       <div className="pb-10 min-h-screen">
-        <Navbar openModal={handleOpenModal} />
         <SettingsModal visible={isOpen} onClose={closeModal} />
-        <>{props.children}</>
+
+        {pageLoading ? (
+          <div className="flex flex-col justify-center items-center h-[90vh] "><div className="container">
+          <div className="red flame"></div>
+          <div className="orange flame"></div>
+          <div className="yellow flame"></div>
+          <div className="white flame"></div>
+          <div className="blue circle"></div>
+          <div className="black circle"></div>
+        </div><p className="text-white -translate-y-4 text-5xl">Loading...</p></div>
+        ) : (
+          <div>{props.children}</div>
+        )}
       </div>
       <Footer />
     </>
